d8d7cdb3ea7a6e52c7d386f90eccb2ceda1425e4
Greg <greglock1997@outlook.com>
2024-08-30 18:40:48 +0100

    Initial commit

---
+++ BlackStarFilmFestival/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className='container'>
+        <div className='header-container'>Blackstar Film Festival, 2024</div>
+        <div className='main-container'>
+          <div className='footer-container'>Blackstar</div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
