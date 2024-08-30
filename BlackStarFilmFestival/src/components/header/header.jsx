f7233f7c155b6befcc6ae7ca82529c4fd9117aad
Greg <greglock1997@outlook.com>
2024-08-30 19:30:13 +0100

    Initial styling for header

--- BlackStarFilmFestival/src/components/header/header.jsx
+++ BlackStarFilmFestival/src/components/header/header.jsx
@@ -1,7 +1,11 @@
+import './header.css';
+
 export default function Header() {
     return (
         <>
-            <div className='header-container'>Blackstar Film Festival, 2024</div>
+            <div className='header-container'>
+                <h3>Blackstar Film Festival <b>2024</b></h3>
+            </div>
         </>
     )
 }
