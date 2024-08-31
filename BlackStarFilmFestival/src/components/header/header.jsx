37d47980fd8cfcf86add35ebac35168beae41bfd
Greg <greglock1997@outlook.com>
2024-08-31 11:48:08 +0100

    Styling changes to header

--- BlackStarFilmFestival/src/components/header/header.jsx
+++ BlackStarFilmFestival/src/components/header/header.jsx
@@ -3,9 +3,13 @@ import './header.css';
 export default function Header() {
     return (
         <>
-            <div className='header-container'>
-                <h3>Blackstar Film Festival <b>2024</b></h3>
-            </div>
+            <header className='header-container'>
+                <h3>
+                    <span className="line-1">Blackstar </span><br />
+                    <span className="line-2">Film Festival </span><br />
+                    <span className="line-3">2024</span>
+                </h3>
+            </header>
         </>
     )
 }
