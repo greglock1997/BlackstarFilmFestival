cd8b652cc2ebd3bbaec914f496efd278c164863e
Greg <greglock1997@outlook.com>
2024-08-31 12:21:18 +0100

    Add responsive styling for mobile devices

--- BlackStarFilmFestival/src/components/header/header.jsx
+++ BlackStarFilmFestival/src/components/header/header.jsx
@@ -5,8 +5,8 @@ export default function Header() {
         <>
             <header className='header-container'>
                 <h3>
-                    <span className="line-1">Blackstar </span><br />
-                    <span className="line-2">Film Festival </span><br />
+                    <span className="line-1">Blackstar </span>
+                    <span className="line-2">Film Festival </span>
                     <span className="line-3">2024</span>
                 </h3>
             </header>
