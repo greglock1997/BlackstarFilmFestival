a13c172422e0dde48510c3d7865eadf655be10a4
Greg <greglock1997@outlook.com>
2024-08-31 18:29:40 +0100

    Add filmCard component

---
+++ BlackStarFilmFestival/src/components/filmCard/filmCard.jsx
@@ -0,0 +1,37 @@
+import './filmCard.css';
+
+export default function FilmCard({ film = {} }) {
+    const imageHtml = film.featured_image_rendered?.card || '';
+    const title = film.title?.rendered || 'No Title';
+
+    return (
+        <div className='filmCard'>
+            {/* Couldn't figure out a way to extract an image src from the API data, refactor later */}
+            {imageHtml ? (
+                // Render image using the HTML provided by API data
+                <div className='filmCard-image' dangerouslySetInnerHTML={{ __html: imageHtml }} />
+            ) : (
+                <h1>Image not found</h1>
+            )}
+            <div className='filmCard-info'>
+                <h1 className='filmCard-title'>{title}</h1>
+                <p>{film.type}</p>
+                <p>{film.acf.credits[0].name}</p>
+                <p>Runtime: {film.acf.runtime}</p>
+            </div>
+            <div className='filmCard-links-acessibility'>
+                <div className='filmCard-links'>
+                    {/* Add links at later point */}
+                    <a href=''>Read More</a>
+                    <a href=''>Watch Trailer</a>
+                </div>
+                <div className='filmCard-accessibility'>
+                    <div><p>AD</p></div>
+                    <div><p>CC</p></div>
+                    <div><p>!</p></div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
--- BlackStarFilmFestival/src/components/main/main.jsx
+++ BlackStarFilmFestival/src/components/main/main.jsx
@@ -1,14 +1,23 @@
 import Footer from '../footer/footer.jsx';
+import FilmCard from '../filmCard/filmCard.jsx';
 import './main.css';
 
-export default function Main() {
+export default function Main({filmData}) {
     return (
         <>
-            <a>Films A-Z</a>
             <main className='main-container'>
-                <header className='main-header'>FILMS A-Z <img src="src\assets\filter.png" alt="Results Filter"></img></header>
+                <header className='main-header'>FILMS A-Z <img src="src/assets/filter.png" alt="Results Filter" /></header>
+                <div className='main-films'>
+                    {filmData && filmData.length > 0 ? (
+                        filmData.map((film) => (
+                            <FilmCard film={film}/>
+                        ))
+                    ) : (
+                        <p>Loading...</p>
+                    )}
+                </div>
                 <Footer />
             </main>
         </>
-    )
+    );
 }
