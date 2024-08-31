50482bdaef59e40d656511512ccb2f675fab8a97
Greg <greglock1997@outlook.com>
2024-08-31 14:37:50 +0100

    Add basic API call, log film data

--- BlackStarFilmFestival/src/App.jsx
+++ BlackStarFilmFestival/src/App.jsx
@@ -1,11 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Header from './components/header/header.jsx';
 import Main from './components/main/main.jsx';
-import Footer from './components/footer/footer.jsx';
+
+
 
 function App() {
-  // const [count, setCount] = useState(0);
+  const [filmData, setFilmData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://wp.blackstarfest.org/wp-json/wp/v2/festival-film?per_page=9&page=1&_year=2024&rich=1&not_hidden=1');
+        
+        const data = await response.json();
+        
+        console.log(data);
+        
+        setFilmData(data);
+      } catch (error) {
+        console.log('Error retrieving data from API', error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <>
--- BlackStarFilmFestival/src/components/main/main.jsx
+++ BlackStarFilmFestival/src/components/main/main.jsx
@@ -4,7 +4,9 @@ import './main.css';
 export default function Main() {
     return (
         <>
+            <a>Films A-Z</a>
             <main className='main-container'>
+                <header className='main-header'>FILMS A-Z <img src="src\assets\filter.png" alt="Results Filter"></img></header>
                 <Footer />
             </main>
         </>
